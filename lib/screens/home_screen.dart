a2ca53e1fddace01098f29de790cb21b73b3d66e
Abir Hossain <abir2526@gmail.com>
2023-08-04 06:40:44 +0600

    Added Image Scan and Animation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,8 @@ import '../constants/constants.dart';
 import '../services/image_to_text_service.dart';
 import '../widgets/image_scanning_animation_widget.dart';
 import '../widgets/prompt_input_widget.dart';
+import '../widgets/task_card_ocr_summary_widget.dart';
+import '../widgets/task_card_widget_half_width.dart';
 import '../widgets/text_widget.dart';
 import 'chat_screen.dart';
 
@@ -40,7 +42,73 @@ class _HomeScreenState extends State<HomeScreen> {
                   SingleChildScrollView(
                       child: Column(
                     children: [
-                      HomeScreenTopSection(),
+                      // HomeScreenTopSection(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Flexible(
+                            child: TaskCardSummaryWidget(onPressed: () async {
+                              int? selectedImgSrc =
+                                  await ImageToTextService.getImageSrc(context);
+
+                              if (selectedImgSrc != null) {
+                                setState(() {
+                                  imageProcessing = true;
+                                });
+
+                                String? imageText =
+                                    await ImageToTextService.getTextFromImage(
+                                        selectedImgSrc);
+
+                                setState(() {
+                                  imageProcessing = false;
+                                });
+
+                                if (imageText != null) {
+                                  List<Map<String, dynamic>> conversation = [];
+                                  conversation.add({
+                                    "msg": "Summarize this text\n$imageText",
+                                    "index": 0
+                                  });
+
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ChatScreen(
+                                              conversation: conversation,
+                                              id: 0,
+                                              dateTime: '',
+                                              gobackPageIndex: 0)));
+                                }
+                              }
+                            }),
+                          ),
+                          Flexible(
+                            child: Column(
+                              children: [
+                                for (var cardmodel in card_list)
+                                  TaskCardHalfWidth(
+                                    taskModel: cardmodel,
+                                    onPressed: () {
+                                      List<Map<String, dynamic>> conversation =
+                                          [];
+                                      conversation.add(
+                                          {"msg": cardmodel.msg, "index": 0});
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => ChatScreen(
+                                                  conversation: conversation,
+                                                  id: 0,
+                                                  dateTime: '',
+                                                  gobackPageIndex: 0)));
+                                    },
+                                  ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                       TaskCardSectionListWidget(),
                     ],
                   )),
--- lib/widgets/home_screen_top_section.dart
+++ lib/widgets/home_screen_top_section.dart
@@ -1,61 +1,95 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:smart_gpt_ai/models/half_width_task_card_model.dart';
-import '../screens/chat_screen.dart';
-import '../services/image_to_text_service.dart';
-import 'task_card_ocr_summary_widget.dart';
-import 'task_card_widget_half_width.dart';
+// import '../screens/chat_screen.dart';
+// import '../services/image_to_text_service.dart';
+// import 'task_card_ocr_summary_widget.dart';
+// import 'task_card_widget_half_width.dart';
 
-class HomeScreenTopSection extends StatelessWidget {
-  const HomeScreenTopSection({super.key});
+// class HomeScreenTopSection extends StatefulWidget {
+//   const HomeScreenTopSection({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Flexible(
-          child: TaskCardSummaryWidget(onPressed: () async {
-            await textFromImage(context);
-          }),
-        ),
-        Flexible(
-          child: Column(
-            children: [
-              for (var cardmodel in card_list)
-                TaskCardHalfWidth(
-                  taskModel: cardmodel,
-                  onPressed: () {
-                    List<Map<String, dynamic>> conversation = [];
-                    conversation.add({"msg": cardmodel.msg, "index": 0});
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ChatScreen(
-                                conversation: conversation,
-                                id: 0,
-                                dateTime: '',
-                                gobackPageIndex: 0)));
-                  },
-                ),
-            ],
-          ),
-        )
-      ],
-    );
-  }
+//   @override
+//   State<HomeScreenTopSection> createState() => _HomeScreenTopSectionState();
+// }
 
-  Future<String?> textFromImage(BuildContext context) async {
-    String? imageText;
-    // ImageToTextService imageToTextService = ImageToTextService();
-    int? selectedImgSrc = await ImageToTextService.getImageSrc(context);
-    if (selectedImgSrc != null) {
-      imageText = await ImageToTextService.getTextFromImage(selectedImgSrc);
+// class _HomeScreenTopSectionState extends State<HomeScreenTopSection> {
+//   bool imageProcessing = false;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       children: [
+//         Flexible(
+//           child: TaskCardSummaryWidget(onPressed: () async {
+//             int? selectedImgSrc = await ImageToTextService.getImageSrc(context);
 
-      print('Image Text $imageText');
-    }
-    return imageText;
-  }
-}
+//             if (selectedImgSrc != null) {
+//               setState(() {
+//                 imageProcessing = true;
+//               });
+
+//               String? imageText =
+//                   await ImageToTextService.getTextFromImage(selectedImgSrc);
+
+//               setState(() {
+//                 imageProcessing = false;
+//               });
+
+//               if (imageText != null) {
+//                 List<Map<String, dynamic>> conversation = [];
+//                 conversation.add(
+//                     {"msg": "Summarize this text\n$imageText", "index": 0});
+
+//                 Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                         builder: (context) => ChatScreen(
+//                             conversation: conversation,
+//                             id: 0,
+//                             dateTime: '',
+//                             gobackPageIndex: 0)));
+//               }
+//             }
+//           }),
+//         ),
+//         Flexible(
+//           child: Column(
+//             children: [
+//               for (var cardmodel in card_list)
+//                 TaskCardHalfWidth(
+//                   taskModel: cardmodel,
+//                   onPressed: () {
+//                     List<Map<String, dynamic>> conversation = [];
+//                     conversation.add({"msg": cardmodel.msg, "index": 0});
+//                     Navigator.pushReplacement(
+//                         context,
+//                         MaterialPageRoute(
+//                             builder: (context) => ChatScreen(
+//                                 conversation: conversation,
+//                                 id: 0,
+//                                 dateTime: '',
+//                                 gobackPageIndex: 0)));
+//                   },
+//                 ),
+//             ],
+//           ),
+//         )
+//       ],
+//     );
+//   }
+
+//   Future<String?> textFromImage(BuildContext context) async {
+//     String? imageText;
+//     // ImageToTextService imageToTextService = ImageToTextService();
+//     int? selectedImgSrc = await ImageToTextService.getImageSrc(context);
+//     if (selectedImgSrc != null) {
+//       imageText = await ImageToTextService.getTextFromImage(selectedImgSrc);
+
+//       print('Image Text $imageText');
+//     }
+//     return imageText;
+//   }
+// }
 
 List<HalfWidthTaskCardModel> card_list = [
   HalfWidthTaskCardModel(
